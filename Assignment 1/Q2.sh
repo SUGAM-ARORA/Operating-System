be5ad55828952d210611c5ca9e9b875f5d4fd811
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2025-01-27 19:38:50 +0530

    Create Q2.sh

---
+++ Assignment 1/Q2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Prompt the user to enter a number
+echo "Enter a number:"
+read number
+
+# Check if the number is even or odd
+if [ $((number % 2)) -eq 0 ]; then
+    echo "$number is even."
+else
+    echo "$number is odd."
+fi
