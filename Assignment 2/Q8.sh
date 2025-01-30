ebf89ceb19d76a40632e6542592e957dbab90b12
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2025-01-30 23:54:33 +0530

    Create Q8.sh

---
+++ Assignment 2/Q8.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "--------------------------------------"
+echo " 🔡 String Length Finder - By Sugam Arora "
+echo "--------------------------------------"
+
+# Read input from user
+read -p "Enter a string: " str
+
+# Find length
+length=${#str}
+
+echo "📌 The length of the string \"$str\" is: $length"
+echo "--------------------------------------"
